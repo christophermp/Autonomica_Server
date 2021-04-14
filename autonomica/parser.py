3bff2ae5a602853df67102e9907ef97090c563a0
Christopher Pettersen <meldre@outlook.com>
2021-04-14 22:55:05 +0200

    Update

--- autonomica/parser.py
+++ autonomica/parser.py
@@ -9,5 +9,18 @@ j = json.loads(content)
 request_url = 'http://10.0.0.111:8090/getconfiguration'
 r = requests.get(request_url).json()
 
+devices = []
+
 for i in r["workspaces"][0]["tasks"]:
     print(i["name"], i['devicesReferenced'])
+    devices.append(i['devicesReferenced'])
+    for key, value in i.items():
+        print(key, '->', value)
+
+print(f'Devices {devices}')
+
+for i in devices:
+    print(i[:])
+
+for item in devices:
+    print(item)
