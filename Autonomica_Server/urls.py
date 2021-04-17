541404443c79136a68267298052432e9f89b1398
Christopher Pettersen <meldre@outlook.com>
2021-04-17 14:31:19 +0200

    Update

--- Autonomica_Server/urls.py
+++ Autonomica_Server/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from autonomica import views
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('img/favicon.ico'))),
 
     # Auth
     path('signup/', views.signupuser, name='signupuser'),
